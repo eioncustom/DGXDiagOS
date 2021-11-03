69724a84cd090bc61b5fb74203e9cffd03cdf7b4
eioncustom <75768903+eioncustom@users.noreply.github.com>
2021-11-02 18:59:02 -0700

    Update inject.sh

--- inject.sh
+++ inject.sh
@@ -5,7 +5,7 @@ ftp_server="4.4.4.4"
 
 if [[ ! -f /usr/sbin/menu.sh ]]; then
 echo "Field Diag Menu Does Not Exist, Adding Now"
-sudo wget --directory-prefix=/usr/sbin/ ftp://$ftp_server/menu.sh
+sudo wget -q --directory-prefix=/usr/sbin/ ftp://$ftp_server/menu.sh
 sudo chmod +x /usr/sbin/menu.sh
 else
 echo "Field Diag Menu Already Exists, No Changes Made" 
